521dd43a107a27878992153d69060d61a6261783
RudraUX <pollabrock@gmail.com>
2022-05-06 00:55:45 +0600

    unused library removed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://todos-app-rudraux.hasura.app/v1/graphql',
